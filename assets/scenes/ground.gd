a4aa0815ca156fcd266c3716d767b02c66abc4b7
Joseph Baker <jrb081401@yahoo.com>
2025-09-24 21:47:02 -0400

    Basic code to check location of mouse without running it every frame

--- assets/scenes/ground.gd
+++ assets/scenes/ground.gd
@@ -1,6 +1,5 @@
 extends TileMapLayer
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -8,8 +7,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	print(get_local_mouse_position())
+	var lastKnownMousePosition = Vector2.ZERO
+	var currentMousePosition = TileMapLayer.new().map_to_local(get_local_mouse_position())
+	if(_checkMouseMove(lastKnownMousePosition, currentMousePosition)):
+		print(currentMousePosition)
 	pass
 
 func _input(event):
 	print(event)
+	
+func _checkMouseMove(previousPosition, currentPosition) -> bool:
+	if(previousPosition != currentPosition):
+		return true
+	return false
