9181f837ff378e5ec9a8fa354f6e315a73a36f7d
Joseph Baker <jrb081401@yahoo.com>
2025-09-24 21:30:25 -0400

    Created a bigger starting area for testing of mechanics

--- assets/scenes/ground.gd
+++ assets/scenes/ground.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	_input(get_local_mouse_position())
+	print(get_local_mouse_position())
 	pass
 
 func _input(event):
